bd48f3e8880a2c904fb5fe8e7a29cea7f282184f
Romulo Augusto <romulo.acf@gmail.com>
2020-01-04 18:22:59 +0000

    impressao de randons

--- Projeto-Teste/Program.cs
+++ Projeto-Teste/Program.cs
@@ -7,12 +7,21 @@ namespace Projeto_Teste {
             Console.WriteLine("Oi tutupom");
             
             Random rnd = new Random();
-            int x = rnd.Next();            
+            int x = rnd.Next(1, 500);
+
+            int[] vet = new int[x];
+
             int count = 0;
             //Console.WriteLine("number" + x);
 
             for (int i = 0; i < x; i++) {
                 count++;
+                int y = rnd.Next();
+                vet[i] = y;
+            }
+
+            foreach (int obj in vet) {
+                Console.WriteLine(obj);
             }
 
             Console.WriteLine("contador" + count);
